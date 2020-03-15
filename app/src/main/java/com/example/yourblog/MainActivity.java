e523bc2559d19e7720b2c313fa8803c70ef1d17e
maddurimaheshvardhan <mvmahesh1998@gmail.com>
2020-03-15 16:23:19 +0530

    Website To Android Application

---
+++ app/src/main/java/com/example/yourblog/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.yourblog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+    public WebView mywebview;
+    private InterstitialAd mInterstitialAd;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebview=(WebView)findViewById(R.id.webview);
+        WebSettings webSettings=mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebview.loadUrl("https://rangingspeed.blogspot.com/");
+        //opening app
+        mywebview.setWebViewClient(new WebViewClient());
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-2843002830226159/1273133541");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+    }
+    public void ShowAd(View v){
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAG", "The interstitial wasn't loaded yet.");
+        }
+    }
+    //back button
+
+    @Override
+    public void onBackPressed() {
+        if (mywebview.canGoBack()) {
+            mywebview.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
